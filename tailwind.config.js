6366d606e8ea6c1e9f724b722618e21bd461e06f
MohammadSharifAnsari <mohammadsharifansari157@gmail.com>
2023-10-03 12:50:05 +0530

    tailwind responcive clone complete

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content ./dist/*.html men dist me jitni bhi html file hai unpar tailwind laga do
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
